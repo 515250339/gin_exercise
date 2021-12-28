685f275c8e28c471b0d7dd30ae406b6c1751d812
wyx <515250339@qq.com>
2021-12-28 23:16:25 +0800

    feature: 新增注释

--- main.go
+++ main.go
@@ -9,9 +9,13 @@ func main() {
 			"message": "hello gin",
 		})
 	})
+	// GET 请求
 	r.GET("/path/:id", func(c *gin.Context) {
+		// 获取 url 参数
 		id := c.Param("id")
+		// 获取 params 参数，默认为 wang
 		user := c.DefaultQuery("user", "wang")
+		// 获取 params 参数
 		pwd := c.Query("pwd")
 
 		c.JSON(200, gin.H{
@@ -20,14 +24,18 @@ func main() {
 			"pwd":  pwd,
 		})
 	})
+	// POST 请求
 	r.POST("/path", func(c *gin.Context) {
+		// 获取请求体中参数 默认为 wang
 		user := c.DefaultPostForm("user", "wang")
+		// 获取请求体中参数
 		pwd := c.PostForm("pwd")
 		c.JSON(200, gin.H{
 			"user": user,
 			"pwd":  pwd,
 		})
 	})
+	// PUT 请求
 	r.PUT("/path", func(c *gin.Context) {
 		user := c.DefaultPostForm("user", "wang")
 		pwd := c.PostForm("pwd")
@@ -36,6 +44,7 @@ func main() {
 			"pwd":  pwd,
 		})
 	})
+	// DELETE 请求
 	r.DELETE("/path/:id", func(c *gin.Context) {
 		id := c.Param("id")
 		user := c.DefaultPostForm("user", "wang")
