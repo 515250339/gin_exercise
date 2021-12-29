d7213303ac7ef3c915336b14619163421eb51945
wyx <515250339@qq.com>
2021-12-29 21:45:19 +0800

    feature: 请求参数校验的方式

--- main.go
+++ main.go
@@ -1,13 +1,95 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
+)
+
+type Postparams1 struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+	Sex  bool   `json:"sex"`
+}
+
+type Postparams2 struct {
+	Name string `json:"name" uri:"name"`
+	Age  int    `json:"age" uri:"age"`
+	Sex  bool   `json:"sex" uri:"sex"`
+}
+
+type Postparams3 struct {
+	Name string `json:"name" form:"name" binding:"required"`
+	Age  int    `json:"age" form:"age" binding:"required,mustBig"`
+	Sex  bool   `json:"sex" form:"sex" binding:"required"`
+}
+
+func mustBig(f1 validator.FieldLevel) bool {
+	if f1.Field().Interface().(int) <= 18 {
+		return false
+	}
+	return true
+}
 
 func main() {
 	r := gin.Default() // 携带基础中间件启动
+	// 参数校验 age 是否大于 18
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("mustBig", mustBig)
+	}
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "hello gin",
 		})
+	})
+	// 通过 ShouldBindQuery 验证参数
+	r.POST("/testBind3", func(c *gin.Context) {
+		var p Postparams3
+		err := c.ShouldBindQuery(&p)
+		if err != nil {
+			c.JSON(200, gin.H{
+				"msg":   "失败了",
+				"data":  gin.H{},
+				"error": err.Error(),
+			})
+		} else {
+			c.JSON(200, gin.H{
+				"msg":  "成功了",
+				"data": p,
+			})
+		}
+	})
+	// 通过 ShouldBindUri 验证参数
+	r.POST("/testBind2/:name/:age/:sex", func(c *gin.Context) {
+		var p Postparams2
+		err := c.ShouldBindUri(&p)
+		if err != nil {
+			c.JSON(200, gin.H{
+				"msg":  "失败了",
+				"data": gin.H{},
+			})
+		} else {
+			c.JSON(200, gin.H{
+				"msg":  "成功了",
+				"data": p,
+			})
+		}
+	})
+	// 通过 ShouldBindJSON 验证参数
+	r.POST("/testBind1", func(c *gin.Context) {
+		var p Postparams1
+		err := c.ShouldBindJSON(&p)
+		if err != nil {
+			c.JSON(200, gin.H{
+				"msg":  "失败了",
+				"data": gin.H{},
+			})
+		} else {
+			c.JSON(200, gin.H{
+				"msg":  "成功了",
+				"data": p,
+			})
+		}
 	})
 	// GET 请求
 	r.GET("/path/:id", func(c *gin.Context) {
